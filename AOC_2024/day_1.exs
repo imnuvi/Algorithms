fe08d940b423a5f6273921de29159b847f4296ea
Ram  Prakash <ramprakashsmail@gmail.com>
2024-12-01 15:25:23 -0500

    ADD: day 1 part 2 for AOC 2024

--- AOC_2024/day_1.exs
+++ AOC_2024/day_1.exs
@@ -17,8 +17,18 @@ defmodule Solution do
       |> Enum.map(fn x -> Enum.map(x, fn y -> Integer.parse(y) end ) end)
       |> Enum.map(fn x -> Enum.sort(Enum.map(x, fn {y, _} -> y end )) end)
 
-    Enum.zip(Enum.at(trans, 0), Enum.at(trans, -1))
-      |> Enum.reduce(0, fn x, acc -> acc + abs(elem(x, 0) - elem(x, 1)) end)
+    list1 = Enum.at(trans, 0)
+    list2 = Enum.at(trans, -1)
+
+    case part do
+      :one ->
+        Enum.zip(list1, list2)
+          |> Enum.reduce(0, fn x, acc -> acc + abs(elem(x, 0) - elem(x, 1)) end)
+
+      :two ->
+        list1
+          |> Enum.reduce(0, fn x, finacc -> finacc + (x * Enum.reduce(list2, 0, fn y, acc -> if x == y, do: acc + 1, else: acc end)) end)
+    end
 
   end
 end
