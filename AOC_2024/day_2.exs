5639f345ce552c8250364c7a7ca444f8b30c1784
Ram  Prakash <ramprakashsmail@gmail.com>
2024-12-02 23:40:34 -0500

    ADD: partially working code for day 2

--- AOC_2024/day_2.exs
+++ AOC_2024/day_2.exs
@@ -1,5 +1,11 @@
+# https://adventofcode.com/2024/day/2
 
 defmodule Solution do
+  def check_safety(safety_list, mindiff) do
+      chunker = Enum.chunk_every(safety_list, 2, 1, :discard)
+      Enum.all?(Enum.map(chunker, fn [v1, v2] -> (v1 > v2) and (abs(v1 - v2) <= mindiff) end)) or Enum.all?(Enum.map(chunker, fn [v1, v2] -> (v1 < v2) and (abs(v1 - v2) <= mindiff) end))
+  end
+
   def run(file, part) do
     {_, res} = File.read(file)
     mindiff = 3
@@ -13,22 +19,27 @@ defmodule Solution do
         end
         )
       end)
-      |> Enum.map(fn x -> Enum.chunk_every(x, 2, 1, :discard) end)
-      |> Enum.map(fn x -> [Enum.map(x, fn [v1, v2] -> (v1 > v2) and (abs(v1 - v2) <= mindiff) end), Enum.map(x, fn [v1, v2] -> (v1 < v2) and (abs(v1 - v2) <= mindiff) end)] end)
 
     case part do
-      :one ->
-        processed
-          |> Enum.map(fn [l1, l2] -> Enum.all?(l1) or Enum.all?(l2) end )
-          |> Enum.count(fn x -> x == true end)
-
       :two ->
         processed
-          |> Enum.map(fn [l1, l2] -> Enum.all?(l1) or Enum.all?(l2) or Enum.count(l1, fn x -> x == false end) == 1  or Enum.count(l2, fn x -> x == false end) == 1 end )
+          |> Enum.map(fn y ->
+             Enum.map(y, fn x -> check_safety(List.delete(y, x), mindiff) end )
+          end)
+          |> Enum.map(fn x -> Enum.any?(x) end )
+          |> Enum.count(fn x -> x == true end)
+
+      :one ->
+        processed
+          # |> Enum.map(fn x -> Enum.chunk_every(x, 2, 1, :discard) end)
+          # |> Enum.map(fn x -> [Enum.map(x, fn [v1, v2] -> (v1 > v2) and (abs(v1 - v2) <= mindiff) end), Enum.map(x, fn [v1, v2] -> (v1 < v2) and (abs(v1 - v2) <= mindiff) end)] end)
+          # |> Enum.map(fn [l1, l2] -> Enum.all?(l1) or Enum.all?(l2) end )
+          |> Enum.map(fn x -> check_safety(x, mindiff) end)
           |> Enum.count(fn x -> x == true end)
     end
   end
 end
 
 IO.inspect(Solution.run("./inputs/day_2.txt", :one))
+# Solution.run("./inputs/day_2.txt", :one)
 IO.inspect(Solution.run("./inputs/day_2.txt", :two))
